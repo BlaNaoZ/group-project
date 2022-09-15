5910bf8cbd80e986695931fa73a8d1bb1c2d334c
Andrei Blanaru <florian-andrei.blanaru@kainos.com>
2022-09-15 10:19:15 +0100

    Added working API.

--- FullStack/src/main/java/com/kainos/ea/WebServiceApplication.java
+++ FullStack/src/main/java/com/kainos/ea/WebServiceApplication.java
@@ -1,10 +1,22 @@
 package com.kainos.ea;
 
+import com.kainos.ea.resources.APIService;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
 public class WebServiceApplication extends Application<WebServiceConfiguration> {
+
+    private static Connection conn;
 
     public static void main(final String[] args) throws Exception {
         new WebServiceApplication().run(args);
@@ -15,15 +27,70 @@ public class WebServiceApplication extends Application<WebServiceConfiguration> 
         return "WebService";
     }
 
-    @Override
     public void initialize(final Bootstrap<WebServiceConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(new SwaggerBundle<WebServiceConfiguration>() {
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(WebServiceConfiguration configuration) {
+                return configuration.getSwagger();
+            }
+        });
     }
 
     @Override
     public void run(final WebServiceConfiguration configuration,
                     final Environment environment) {
-        // TODO: implement application
+        environment.jersey().register(new APIService());
+
+
+        try {
+            Connection con = getConnection();
+            Statement st = con.createStatement();
+            st.execute("USE full_RaduG");
+
+            //example
+//            ResultSet rs = st.executeQuery("SELECT * FROM Employee");
+//            while(rs.next()) {
+//                System.out.println(rs.getString("fname"));
+//            }
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Bad practice alert!
+        }
+
     }
 
+    public static Connection getConnection() {
+        String user;
+        String password;
+        String host;
+
+        if (conn != null) {
+            return conn;
+        }
+
+        try {
+            FileInputStream propsStream =
+                    new FileInputStream("src/main/resources/properties");
+
+            Properties props = new Properties();
+            props.load(propsStream);
+
+            user            = props.getProperty("user");
+            password        = props.getProperty("password");
+            host            = props.getProperty("host");
+
+            if (user == null || password == null || host == null)
+                throw new IllegalArgumentException(
+                        "Properties file must exist and must contain "
+                                + "user, password, and host properties.");
+
+            conn = DriverManager.getConnection("jdbc:mysql://"
+                    + host + "/", user, password);
+            return conn;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
---
+++ FullStack/src/main/java/com/kainos/ea/resources/APIService.java
@@ -0,0 +1,186 @@
+package com.kainos.ea.resources;
+
+
+import com.kainos.ea.WebServiceApplication;
+import io.swagger.annotations.Api;
+import org.eclipse.jetty.util.ajax.JSON;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/api")
+@Api("Engineering Academy Dropwizard API")
+public class APIService {
+
+    @GET
+    @Path("/report/finance")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getFinanceEmployees() {
+        try {
+            Connection con = WebServiceApplication.getConnection();
+            Statement st = con.createStatement();
+            //st.execute("USE ddata_LauraP");
+            ResultSet rs = st.executeQuery("Select * from Employee where department = 'FINANCE'");
+            List<String> allHREmployees = new ArrayList();
+            while (rs.next()) {
+                String number = String.valueOf(rs.getInt("number"));
+                String fname = rs.getString("fname");
+                String lname = rs.getString("lname");
+                String post = rs.getString("postcode");
+                String address = rs.getString("address");
+                String nin = rs.getString("nin");
+                String bank_account = rs.getString("bank_account");
+                String  starting_salary = String.valueOf(rs.getInt("starting_salary"));
+                String isManager = String.valueOf(rs.getInt("isManager"));
+                String department = rs.getString("department");
+
+                String total = number + " " + fname + " "
+                        + lname + " " +post + " " + address +" " + nin
+                        + " " + bank_account + " " + starting_salary+" "+
+                         isManager + " " + department;
+                allHREmployees.add(total);
+
+            }
+
+            return JSON.toString(allHREmployees);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+    }
+
+    @GET
+    @Path("/report/sales")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getSalesEmployees() {
+        try {
+            Connection con = WebServiceApplication.getConnection();
+            Statement st = con.createStatement();
+            //st.execute("USE ddata_LauraP");
+            ResultSet rs = st.executeQuery("Select * from Employee where department = 'SALES'");
+            List<String> allHREmployees = new ArrayList();
+            while (rs.next()) {
+                String number = String.valueOf(rs.getInt("number"));
+                String fname = rs.getString("fname");
+                String lname = rs.getString("lname");
+                String post = rs.getString("postcode");
+                String address = rs.getString("address");
+                String nin = rs.getString("nin");
+                String bank_account = rs.getString("bank_account");
+                String  starting_salary = String.valueOf(rs.getInt("starting_salary"));
+                String isManager = String.valueOf(rs.getInt("isManager"));
+                String department = rs.getString("department");
+
+                String total = number + " " + fname + " "
+                        + lname + " " +post + " " + address +" " + nin
+                        + " " + bank_account + " " + starting_salary+" "+
+                        isManager + " " + department;
+                allHREmployees.add(total);
+
+            }
+
+            return JSON.toString(allHREmployees);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @GET
+    @Path("/report/hr")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getHREmployees() {
+        try {
+            Connection con = WebServiceApplication.getConnection();
+            Statement st = con.createStatement();
+            //st.execute("USE ddata_LauraP");
+            ResultSet rs = st.executeQuery("Select * from Employee where department = 'HR'");
+            List<String> allHREmployees = new ArrayList();
+            while (rs.next()) {
+                String number = String.valueOf(rs.getInt("number"));
+                String fname = rs.getString("fname");
+                String lname = rs.getString("lname");
+                String post = rs.getString("postcode");
+                String address = rs.getString("address");
+                String nin = rs.getString("nin");
+                String bank_account = rs.getString("bank_account");
+                String  starting_salary = String.valueOf(rs.getInt("starting_salary"));
+                String isManager = String.valueOf(rs.getInt("isManager"));
+                String department = rs.getString("department");
+
+                String total = number + " " + fname + " "
+                        + lname + " " +post + " " + address +" " + nin
+                        + " " + bank_account + " " + starting_salary+" "+
+                        isManager + " " + department;
+                allHREmployees.add(total);
+
+            }
+
+            return JSON.toString(allHREmployees);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+        @POST
+    @Path("/enter/employee")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String enterEmployee(Message message) {
+        // Return a string for testing purposes
+
+        // TODO: use the fields of 'message' to add an employee to the database
+        String fname = message.getFname();
+        String lname = message.getLname();
+        String address = message.getAddress();
+        String postcode = message.getPostcode();
+        String nin = message.getNin();
+        String bankAccount = message.getBankAccount();
+        String startingSalary = String.valueOf(message.getStartingSalary());
+        boolean isManager = true;
+        String department = String.valueOf(message.getDepartment());
+
+
+
+        Connection con = WebServiceApplication.getConnection();
+        try {
+            String sql = "INSERT INTO Employee (fname, lname, postcode, address, nin," +
+                    "bank_account, starting_salary, isManager, department) VALUES " +
+                    "(?,?, ?, ?, ?, ?, ?, ?, ?);";
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, fname);
+            statement.setString(2, lname);
+            statement.setString(3, postcode);
+            statement.setString(4, address);
+            statement.setString(5, nin);
+            statement.setString(6, bankAccount);
+            statement.setString(7, startingSalary);
+            statement.setBoolean(8, isManager);
+            statement.setString(9, department);
+            statement.execute();
+            return "Data inserted successfully";
+        }
+        catch (SQLException e) {
+            // Bad practice :)
+            //TODO: actually handle this exception
+            e.printStackTrace();
+        }
+        catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+
+        return  "Something went wrong. Database not updated.";
+    }
+
+
+}
